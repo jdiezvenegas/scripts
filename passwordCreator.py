b57cc142fe8be39175d3794f13c5ad5dfe9e919a
jdiezvenegas <100379203+jdiezvenegas@users.noreply.github.com>
2022-03-02 01:23:37 +0100

    Update passwordCreator.py

--- passwordCreator.py
+++ passwordCreator.py
@@ -1,14 +1,16 @@
 import random
 import os
 import sys
-contraseña=""
-long=int(input("Introduce la longitud deseada de contraseña:\n"))
-for i in range(0,long):
-    ran=random.randint(32,254)
-    if ran==127:
-        i=i-1
-        continue
-    letra=chr(ran)
-    contraseña=contraseña+letra
-    print(str(ran)+" "+letra)
-print(contraseña)
+import string
+print("Establezca el tamaño de la contraseña:")
+tam=input("Por defecto es aleatorio, con una longitud entre 8 y 32 carácteres:\n")
+if tam=="" or not tam.isnumeric():
+    print("Longitud aleatoria\n")
+    tam=random.randint(8,32)
+sim=input("¿Desea símbolos extraños? [S/n]\n")
+tam=int(tam)
+if sim=="" or sim=="S" or sim=="s":
+    randomstr = ''.join(random.choices(string.ascii_letters+string.digits+string.punctuation+"ñ"+"@"+" ",k=tam))
+else:
+    randomstr = ''.join(random.choices(string.ascii_letters+string.digits,k=tam))
+print(randomstr)
